e6ec0e2edeecfde7dfcacf02bced266b43a60b11
valentinerutto <rutto28@gmail.com>
2024-05-03 07:05:30 +0300

    -add cube pager animation

--- app/src/main/java/com/valentinerutto/roomdbtutorial/ui/LineItemComposable.kt
+++ app/src/main/java/com/valentinerutto/roomdbtutorial/ui/LineItemComposable.kt
@@ -3,12 +3,15 @@ package com.valentinerutto.roomdbtutorial.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -16,21 +19,24 @@ import com.valentinerutto.roomdbtutorial.data.local.PickuplineEntity
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
-fun LineItemComposable(modifier: Modifier,  entity: PickuplineEntity) {
+fun LineItemComposable(modifier: Modifier, entity: PickuplineEntity) {
     val vm = koinViewModel<LineViewModel>()
     Card(
-        modifier = Modifier.padding(16.dp),
-        shape = RoundedCornerShape(16.dp),
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .wrapContentSize(align = Alignment.Center)
+            .height(300.dp), shape = RoundedCornerShape(16.dp)
+
     ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(2.dp)
+                .padding(16.dp)
         ) {
-            Spacer(modifier = Modifier.padding(2.dp))
-            Text(text = entity.text!!, fontSize = 16.sp)
-            Spacer(modifier = Modifier.padding(1.dp))
-
+            Spacer(modifier = Modifier.padding(4.dp))
+            Text(text = entity.text, fontSize = 16.sp)
+            Spacer(modifier = Modifier.padding(4.dp))
             Button(onClick = {
 
                 vm.deleteLine(entity)
--- app/src/main/java/com/valentinerutto/roomdbtutorial/ui/MainView.kt
+++ app/src/main/java/com/valentinerutto/roomdbtutorial/ui/MainView.kt
@@ -1,14 +1,18 @@
 package com.valentinerutto.roomdbtutorial.ui
 
+import androidx.compose.animation.core.FastOutLinearInEasing
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.PagerDefaults
+import androidx.compose.foundation.pager.PagerSnapDistance
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -20,35 +24,115 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.BlurredEdgeTreatment
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.valentinerutto.roomdbtutorial.data.local.PickuplineEntity
 import kotlinx.coroutines.launch
+import kotlin.math.absoluteValue
+import kotlin.math.min
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MainView(
     modifier: Modifier = Modifier, lines: List<PickuplineEntity>
 ) {
+
     Carousel(modifier = modifier.fillMaxSize(), lines = lines)
 
 }
-
+//https://www.sinasamaki.com/setting-up-viewpager-in-jetpack-compose/
+//https://gist.github.com/sinasamaki/55693586ea97e0e425c22230c18a4784?ref=sinasamaki.com
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun Carousel(modifier: Modifier, lines: List<PickuplineEntity>) {
-    Box(modifier.fillMaxWidth()) {
+    val pagerState = rememberPagerState(pageCount = { lines.size })
 
-        val pagerState = rememberPagerState(pageCount = { lines.size })
-
-        HorizontalPager(state = pagerState, pageSize = PageSize.Fill) {
-            LineItemComposable(modifier = modifier.align(Alignment.Center), entity = lines[it])
+    val pageScale by remember {
+        derivedStateOf {
+            1f - (pagerState.currentPageOffsetFraction.absoluteValue) * .3f
         }
-        PageIndicators(pagerState = pagerState, Modifier.align(Alignment.BottomCenter))
+    }
+
+    Box(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        val offsetFromStart = pagerState.getOffsetFractionForPage(0).absoluteValue
+        Box(modifier = Modifier
+            .aspectRatio(1f)
+            .offset { IntOffset(0, 150.dp.roundToPx()) }
+            .scale(scaleX = .6f, scaleY = .24f)
+            .scale(pageScale)
+            .rotate(offsetFromStart * 90f)
+            .blur(
+                radius = 110.dp,
+                edgeTreatment = BlurredEdgeTreatment.Unbounded,
+            )
+            .background(Color.Black.copy(alpha = .5f)))
+
+        Box(modifier.fillMaxWidth()) {
+
+
+            HorizontalPager(
+                state = pagerState,
+                pageSpacing = 16.dp,
+                flingBehavior = PagerDefaults.flingBehavior(
+                    state = pagerState, pagerSnapDistance = PagerSnapDistance.atMost(0)
+                ),
+                contentPadding = PaddingValues(horizontal = 32.dp),
+                modifier = Modifier
+                    .scale(1f, scaleY = pageScale)
+                    .aspectRatio(1f),
+            ) { index ->
+                Box(modifier = Modifier
+                    .aspectRatio(1f)
+                    .graphicsLayer {
+                        val pageOffset = pagerState.getOffsetFractionForPage(index)
+                        val offScreenRight = pageOffset < 0f
+                        val deg = 105f
+                        val interpolated = FastOutLinearInEasing.transform(pageOffset.absoluteValue)
+                        rotationY = min(interpolated * if (offScreenRight) deg else -deg, 90f)
+
+                        transformOrigin = TransformOrigin(
+                            pivotFractionX = if (offScreenRight) 0f else 1f, pivotFractionY = .5f
+                        )
+                    }
+                    .drawWithContent {
+                        val pageOffset = pagerState.getOffsetFractionForPage(index)
+
+                        this.drawContent()
+                        drawRect(
+                            Color.Black.copy(
+                                (pageOffset.absoluteValue * .7f)
+                            )
+                        )
+                    }
+                    .background(Color.LightGray), contentAlignment = Alignment.Center) {
+                    LineItemComposable(
+                        modifier = modifier.align(Alignment.Center), entity = lines[index]
+                    )
+                }
+            }
+            PageIndicators(pagerState = pagerState, Modifier.align(Alignment.BottomCenter))
+        }
     }
 }
 
