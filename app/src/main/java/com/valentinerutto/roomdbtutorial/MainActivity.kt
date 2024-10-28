e53bb0a1c791ca2396ee3ad530b24871dffe64ac
valentinerutto <valentine.rutto@twiga.com>
2024-10-28 22:07:01 +0300

    -downgrade dependencies

--- app/src/main/java/com/valentinerutto/roomdbtutorial/MainActivity.kt
+++ app/src/main/java/com/valentinerutto/roomdbtutorial/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
